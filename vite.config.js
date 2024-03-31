5eeaa71ad7304bf312597a1723aeec069a9d1317
Erdong Chen <84251107+ErdongChen-Andrew@users.noreply.github.com>
2024-03-31 16:46:12 -0700

    fix: declaration file generation

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,39 @@
-import { defineConfig } from 'vite'
-import * as path from 'node:path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import * as path from "node:path";
+import react from "@vitejs/plugin-react";
 
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+const isCodeSandbox =
+  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
 const dev = defineConfig({
   plugins: [react()],
-  root: 'example/',
-  publicDir: '../public/',
-  base: './',
+  root: "example/",
+  publicDir: "../public/",
+  base: "./",
   server: {
     host: true,
     open: !isCodeSandbox, // Open if it's not a CodeSandbox
   },
-})
+});
 
 const build = defineConfig({
   publicDir: false,
   build: {
     minify: false,
     sourcemap: true,
-    target: 'es2018',
+    target: "es2018",
     lib: {
-      formats: ['cjs', 'es'],
-      entry: 'src/Ecctrl.tsx',
-      fileName: '[name]',
+      formats: ["cjs", "es"],
+      entry: "src/Ecctrl.tsx",
+      fileName: "[name]",
     },
     rollupOptions: {
-      external: (id) => !id.startsWith('.') && !path.isAbsolute(id),
+      external: (id) => !id.startsWith(".") && !path.isAbsolute(id),
       output: {
         sourcemapExcludeSources: true,
       },
     },
   },
-  plugins: [
-    {
-      name: 'vite-tsc',
-      generateBundle(options) {
-        const ext = options.format === 'es' ? 'ts' : 'cts'
-        this.emitFile({ type: 'asset', fileName: `Ecctrl.d.${ext}`, source: `export * from '../src/Ecctrl.tsx'` })
-      },
-    },
-  ]
-})
+});
 
-export default process.argv[2] ? build : dev
+export default process.argv[2] ? build : dev;
